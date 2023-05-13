e8c959a25bd3a04b98b93057f7df363f645ac5e7
Ubuntu <sqa@tv.rirtamz54y3ejep2pbrn4rmx4a.bx.internal.cloudapp.net>
2023-05-13 18:43:46 +0000

    Redeclare channel.h according to Version 1.1.1

--- include/Channel.h
+++ include/Channel.h
@@ -15,6 +15,7 @@ class Channel {
     const int fd;  // Associated file descriptor
     InterestEventSet interestEvents;  // A set of event types which this channel expect to monitor
     int receivedEvent;   // The type of events received from the ring
+    int returnVal;  // The return value of the submitted system call. Usally an int
     int registerFlag;    // Mainly used to indicate if channel has been added to a Ring's ChannelMap
     EventLoop* ownerLoop;  // Associated EventLoop
 
@@ -22,21 +23,28 @@ class Channel {
     Channel(EventLoop* loop, int fd);
     ~Channel();
 
-    /* Enable or disable monitoring read Event*/
-    void enableRead();
-    void disableRead();
+    /* Enable or disable monitoring read() Event*/
+    void enable_read();
+    void disable_read();
 
-    /* Enable or disable monitoring write Event*/
-    void enableWrite();
-    void disableWrite();
+    /* Enable or disable monitoring write() Event*/
+    void enable_write();
+    void disable_write();
+
+    /* Enable or disable monitoring accept() Event*/
+    void enable_accept();
+    void disable_accept();
 
     /* Handle received event*/
-    void handleEvent();
+    void handle_event();
 
-    /* Handle read and write*/
-    void handleRead() const;
-    void handleWrite() const;
+    /* Handle read, write, accept*/
+    void handle_read() const;
+    void handle_write() const;
+    void handle_accept() const;
 
     /* Set receivedEvent number*/
     void set_receivedEvent(int eventType) { receivedEvent = eventType;}
+    void set_returnVal(int result) { returnVal = result;}
+
 };
