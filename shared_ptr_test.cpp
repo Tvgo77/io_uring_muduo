c8dc274d8a8a6b572aee28d98f2831eb0f2f29bb
Ubuntu <sqa@tv.rirtamz54y3ejep2pbrn4rmx4a.bx.internal.cloudapp.net>
2023-05-15 16:05:48 +0000

    A new test for shared_ptr feature

--- shared_ptr_test.cpp
+++ shared_ptr_test.cpp
@@ -4,7 +4,11 @@
 int main() {
     std::vector<std::shared_ptr<int>> vec;
     int x = 100;
+    int * normalPtr = new int(99);
     std::shared_ptr<int> ptr(&x);
+    std::shared_ptr<int> ptr2(ptr.get());
+    std::shared_ptr<int> ptr3(normalPtr);
+    ptr3.reset();
     vec.push_back(ptr);
     vec.push_back(ptr);
     vec.erase(vec.begin()+1);
