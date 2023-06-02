6ac6a3d7ff0e27d6c3a64b2d139df2885d82e504
Ubuntu <sqa@tv.rirtamz54y3ejep2pbrn4rmx4a.bx.internal.cloudapp.net>
2023-06-02 02:53:43 +0000

    Add file channel read test

---
+++ TCPchannel/FileChannel.cc
@@ -0,0 +1,44 @@
+#include <include/EventLoop.h>
+#include <include/Channel.h>
+#include <include/Ring.h>
+#include <vector>
+#include <fcntl.h>
+
+using std::string;
+
+/* To test fileChannel read function, 
+   comment line 187 and uncomment line 188 in Channel.cc.
+   Write the read message to stdout.
+*/
+
+int main() {
+    /* Initialize EventLoop */
+    EventLoop eventLoop;
+
+    /* Channel pointer list */
+    Channel* ChannelPtrList[5] = {nullptr};
+
+
+    /* Files to read */
+    string fileDir("../file/");
+    std::vector<string> fileList = {"f1", "f2", "f3", "f4", "f5"};
+    std::vector<int> fdList(5, -1);  //  Initialize all file descriptor to -1;
+
+    /* Open every file */
+    for (int i = 0; i < fileList.size(); i++) {
+        string filePath = fileDir + fileList[i];
+        fdList[i] = ::open(filePath.c_str(), O_RDWR);
+    }
+
+    /* Allocate Channel for every Opened file */
+    for (int i = 0; i< fdList.size(); i++) {
+        if (fdList[i] > 0) {
+            ChannelPtrList[i] = new Channel(&eventLoop, fdList[i]);
+            ChannelPtrList[i]->enable_read();
+        }
+    }
+
+    /* Start loop*/
+    eventLoop.loop();
+
+}
